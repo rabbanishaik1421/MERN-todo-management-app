0811ed48295c1fe71a1c9decbb5724c0e447d54a
rabbanishaik1421 <rabbanishaik1421@gmail.com>
2026-02-11 14:42:54 +0530

    Vite reactjs installed for front end development Todo list page created with to do form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,65 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-sm-12">
+          <h4 className='text-center mb-5'>Todo App</h4>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="container">
+            <div className="card">
+              <div className="card-header">
+                <h6 className="card-title">Add To do</h6>
+              </div>
+              <div className="card-body">
+                <form action="">
+                  <div className="form-group mb-3">
+                    <label htmlFor="">Enter Title</label>
+                    <input type="text" className="form-control" name="title" id="title" />
+                  </div>
+                  <div className="form-group">
+                    <button className='btn btn-sm btn-primary'>Save</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-8">
+          <div className="card w-100">
+            <div className="card-header">
+              <div className="card-title">To do list</div>        
+            </div>
+            <div className="card-body">
+              <table className="table table-bordered table-hover table-responsive">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Completed</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
